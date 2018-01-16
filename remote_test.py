a7f131725f6320d078cdccabfa70c25cdf558c84
guerrce <guerrerocesar38@gmail.com>
2018-01-16 20:25:49 +0000

    added template

--- remote_test.py
+++ remote_test.py
@@ -1,1 +1,52 @@
-print "testing change"
+
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+# Raspberry Pi pin configuration:
+RST = 24
+
+# 128x64 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+##############################################################
+
+########################
+### Your image here! ###
+########################
+
+
+
+##############################################################
+
+# Display image.
+disp.image(image)
+disp.display()
+
+disp.clear()
+disp.display()
