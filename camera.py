1c64670148122782428645e04227b772553b7702
guerrce <guerrerocesar38@gmail.com>
2018-01-16 15:57:05 -0500

    testing camera picture taking

--- camera.py
+++ camera.py
@@ -3,6 +3,9 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.start_preview()
-sleep(10)
-camera.stop_preview
+  camera.start_preview()
+
+  for i in range(5):
+      sleep(3)
+      camera.capture('/home/pi/Desktop/test_images/image%s.jpg' % i)
+  camera.stop_preview()
