8b12dbc5b75c2c484b4465269bbd92d74d020b3a
guerrce <guerrerocesar38@gmail.com>
2018-01-16 16:24:03 -0500

    testing camer and display together

--- remote_test.py
+++ remote_test.py
@@ -8,6 +8,11 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+from picamera import PiCamera
+
+##############################################################
+
+# Configuring and initiallizing display
 
 # Raspberry Pi pin configuration:
 RST = 24
@@ -22,32 +27,43 @@ disp.begin()
 disp.clear()
 disp.display()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
+# # Create blank image for drawing.
+# # Make sure to create image with mode '1' for 1-bit color.
+# width = disp.width
+# height = disp.height
+# image = Image.new('1', (width, height))
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+# # Get drawing object to draw on image.
+# draw = ImageDraw.Draw(image)
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-##############################################################
-
-########################
-### Your image here! ###
-########################
-
-
-
-# Display image.
-disp.image(image)
-disp.display()
+# # Draw a black filled box to clear the image.
+# draw.rectangle((0,0,width,height), outline=0, fill=0)
 
 ##############################################################
 
+# Configuring and initializing camera
+
+camera = PiCamera()
+
+camera.start_preview()
+
+##############################################################
+
+# The fun bits
+# Test: take picture, convert, display
+# Testing this 5 times
+
+for i in range(5):
+	camera.capture('/home/pi/Desktop/test_images/test_image.jpg' % i)
+	
+	image = Image.open('/home/pi/Desktop/test_images/test_image.jpg').resize((disp.width, disp.height), Image.ANTIALIAS).convert('1')
+	disp.image(image)
+	disp.display()
+
+
+##############################################################
+
+camera.stop_preview()
 
 disp.clear()
 disp.display()
