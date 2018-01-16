2cfec9265a642c753a8f16b80ec82908b1357570
guerrce <guerrerocesar38@gmail.com>
2018-01-16 16:02:04 -0500

    basically Im and idiot

--- camera.py
+++ camera.py
@@ -5,7 +5,8 @@ camera = PiCamera()
 
 camera.start_preview()
 
-  for i in range(5):
-      sleep(3)
-      camera.capture('/home/pi/Desktop/test_images/image%s.jpg' % i)
-  camera.stop_preview()
+for i in range(5):
+    sleep(3)
+    camera.capture('/home/pi/Desktop/test_images/image%s.jpg' % i)
+
+camera.stop_preview()
