59ae4080979430062d491cfb74cbcb519632447b
guerrce <guerrerocesar38@gmail.com>
2018-01-31 15:47:03 +0000

    whatevs

--- remote_test.py
+++ remote_test.py
@@ -7,6 +7,8 @@ import numpy
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
+import RPi.GPIO as GPIO
+
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -32,48 +34,16 @@ disp.begin()
 disp.clear()
 disp.display()
 
-# # Create blank image for drawing.
-# # Make sure to create image with mode '1' for 1-bit color.
-# width = disp.width
-# height = disp.height
-# image = Image.new('1', (width, height))
-
-# # Get drawing object to draw on image.
-# draw = ImageDraw.Draw(image)
-
-# # Draw a black filled box to clear the image.
-# draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-##############################################################
-
 # Configuring and initializing camera
 
 camera = PiCamera()
 
+camera.brightness = 25
 
+#initialize LED conrol
 
-##############################################################
-
-# Test: take picture, convert, make pic array, display pic
-# Testing this 5 times
-
-"""
-for i in range(5):
-	camera.capture('/home/pi/Desktop/test_images/test_image.jpg')
-	
-	image = Image.open('/home/pi/Desktop/test_images/test_image.jpg')
-	ppm_image = image.resize((disp.width, disp.height), Image.ANTIALIAS).convert('1')
-	
-	disp.image(ppm_image)
-	disp.display()
-
-	time.sleep(2)
-
-camera.stop_preview()
-
-disp.clear()
-disp.display()
-"""
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
 
 ##############################################################
 
@@ -94,49 +64,18 @@ input_list = [1 for i in range(128)]
 with open("test_matrix_w.txt", "wb") as weights:
 	pickle.dump(w_matrix, weights)  
 
-with open("test_matrix_b.txt", "wb") as biases:
-	pickle.dump(b_matrix, biases)
-
-with open("test_inputs.txt", "wb") as inputs:
-	pickle.dump(input_list, inputs)
-
 #load arrays
 weights_array = []
 with open("test_matrix_w.txt", "rb") as wm:
 	weights_array = pickle.load(wm)
 
-bias_array = []
-with open("test_matrix_b.txt", "rb") as bm:
-	bias_array = pickle.load(bm)
-
-intput_vector = []
-with open("test_inputs.txt", "rb") as il:
-	intput_vector = pickle.load(il)
-
 
 #display arrays
 weights_image = AIC.array_to_image(weights_array)
-biases_image = AIC.array_to_image(bias_array)
-input_vector_image = AIC.array_to_image([intput_vector])
 
 weights_image.save("weights_pic.bmp")
-biases_image.save("biases_pic.bmp")
-input_vector_image.save("inputs_pic.bmp")
 
 disp.image(weights_image)
-disp.display()
-time.sleep(1)
-
-camera.start_preview()
-time.sleep(120)
-
-camera.stop_preview()
-
-disp.image(biases_image)
-disp.display()
-time.sleep(1)
-
-disp.image(input_vector_image)
 disp.display()
 time.sleep(1)
 
@@ -149,8 +88,18 @@ new_w_array = AIC.image_to_array(weights_image)
 b = numpy.array(weights_array)
 print numpy.array_equal(new_w_array, b)
 
+#turn LED on, then off
+GPIO.output(18, GPIO.HIGH)
+time.sleep(2)
+GPIO.output(18, GPIO.LOW)
+
+
 ##############################################################
 
 disp.clear()
 disp.display()
+
+#camera.stoppreview()
+
+GPIO.cleanup()
 
